874fd06c2be02b8e022250499c02576daed72b2b
uwx <maxine@cybergal.com>
2025-11-01 19:33:17 +0100

    Add error checking

--- ResticBackupHelper/Program.cs
+++ ResticBackupHelper/Program.cs
@@ -5,132 +5,137 @@ using ResticBackupHelper;
 using YamlDotNet.Serialization;
 using YamlDotNet.Serialization.NamingConventions;
 
-Console.WriteLine("Hello, World!");
-
-YamlStructure structure;
-using (var input = new StreamReader(File.OpenRead("backups.yml")))
+try
 {
-    var deserializer = new StaticDeserializerBuilder(new YamlStaticContext())
-        .WithNamingConvention(CamelCaseNamingConvention.Instance)
-        .Build();
+    Console.WriteLine("Hello, World!");
 
-    structure = deserializer.Deserialize<YamlStructure>(input);
-}
-
-Console.WriteLine(structure);
-
-using var emptyDir = new TemporaryDirectory();
-
-foreach (var (mainTag, backup) in structure.Backups)
-{
-    var psi = new ProcessStartInfo
+    YamlStructure structure;
+    using (var input = new StreamReader(File.OpenRead("backups.yml")))
     {
-        FileName = structure.Meta.ResticExecutable,
-        Verb = "runas",
-        Environment =
-        {
-            ["RESTIC_PASSWORD"] = structure.Meta.Password,
-            ["RESTIC_REPOSITORY"] = structure.Meta.Repository,
-            ["RESTIC_PACK_SIZE"] = structure.Meta.PackSize.ToString(),
-        },
-        WorkingDirectory = backup.Path != null
-            ? Environment.ExpandEnvironmentVariables(backup.Path)
-            : Environment.CurrentDirectory,
-    };
+        var deserializer = new StaticDeserializerBuilder(new YamlStaticContext())
+            .WithNamingConvention(CamelCaseNamingConvention.Instance)
+            .Build();
 
-    if (backup.Path != null && !Directory.Exists(backup.Path))
-    {
-        throw new InvalidOperationException("The specified backup path does not exist: " + backup.Path);
+        structure = deserializer.Deserialize<YamlStructure>(input);
     }
 
-    psi.ArgumentList.Add("backup");
+    Console.WriteLine(structure);
 
-    using var filesFrom = new TemporaryFile(".txt");
-    using var excludeFrom = new TemporaryFile(".txt");
-    using var iexcludeFrom = new TemporaryFile(".txt");
+    using var emptyDir = new TemporaryDirectory();
 
-    if (backup.Include.Length > 0)
+    foreach (var (mainTag, backup) in structure.Backups)
     {
-        await using var stream = new StreamWriter(File.OpenWrite(filesFrom.FilePath));
-        foreach (var include in backup.Include)
+        var psi = new ProcessStartInfo
         {
-            await stream.WriteLineAsync(Environment.ExpandEnvironmentVariables(include));
+            FileName = structure.Meta.ResticExecutable,
+            Verb = "runas",
+            Environment =
+            {
+                ["RESTIC_PASSWORD"] = structure.Meta.Password,
+                ["RESTIC_REPOSITORY"] = structure.Meta.Repository,
+                ["RESTIC_PACK_SIZE"] = structure.Meta.PackSize.ToString(),
+            },
+            WorkingDirectory = backup.Path != null
+                ? Environment.ExpandEnvironmentVariables(backup.Path)
+                : Environment.CurrentDirectory,
+        };
+
+        if (backup.Path != null && !Directory.Exists(backup.Path))
+        {
+            throw new InvalidOperationException("The specified backup path does not exist: " + backup.Path);
         }
 
-        psi.ArgumentList.Add("--files-from");
-        psi.ArgumentList.Add(filesFrom.FilePath);
-    }
-    else
-    {
-        psi.ArgumentList.Add(".");
-    }
+        psi.ArgumentList.Add("backup");
 
-    if (backup.Exclude.Length > 0)
-    {
-        await using var stream = new StreamWriter(File.OpenWrite(excludeFrom.FilePath));
-        foreach (var include in backup.Exclude)
+        using var filesFrom = new TemporaryFile(".txt");
+        using var excludeFrom = new TemporaryFile(".txt");
+        using var iexcludeFrom = new TemporaryFile(".txt");
+
+        if (backup.Include.Length > 0)
         {
-            await stream.WriteLineAsync(Environment.ExpandEnvironmentVariables(include));
+            await using var stream = new StreamWriter(File.OpenWrite(filesFrom.FilePath));
+            foreach (var include in backup.Include)
+            {
+                await stream.WriteLineAsync(Environment.ExpandEnvironmentVariables(include));
+            }
+
+            psi.ArgumentList.Add("--files-from");
+            psi.ArgumentList.Add(filesFrom.FilePath);
+        }
+        else
+        {
+            psi.ArgumentList.Add(".");
         }
 
-        psi.ArgumentList.Add("--exclude-file");
-        psi.ArgumentList.Add(excludeFrom.FilePath);
-    }
-
-    if (backup.Iexclude.Length > 0)
-    {
-        await using var stream = new StreamWriter(File.OpenWrite(iexcludeFrom.FilePath));
-        foreach (var include in backup.Iexclude)
+        if (backup.Exclude.Length > 0)
         {
-            await stream.WriteLineAsync(Environment.ExpandEnvironmentVariables(include));
+            await using var stream = new StreamWriter(File.OpenWrite(excludeFrom.FilePath));
+            foreach (var include in backup.Exclude)
+            {
+                await stream.WriteLineAsync(Environment.ExpandEnvironmentVariables(include));
+            }
+
+            psi.ArgumentList.Add("--exclude-file");
+            psi.ArgumentList.Add(excludeFrom.FilePath);
         }
 
-        psi.ArgumentList.Add("--iexclude-file");
-        psi.ArgumentList.Add(iexcludeFrom.FilePath);
-    }
+        if (backup.Iexclude.Length > 0)
+        {
+            await using var stream = new StreamWriter(File.OpenWrite(iexcludeFrom.FilePath));
+            foreach (var include in backup.Iexclude)
+            {
+                await stream.WriteLineAsync(Environment.ExpandEnvironmentVariables(include));
+            }
 
-    psi.ArgumentList.Add("--tag");
-    psi.ArgumentList.Add(mainTag);
+            psi.ArgumentList.Add("--iexclude-file");
+            psi.ArgumentList.Add(iexcludeFrom.FilePath);
+        }
 
-    foreach (var tag in backup.Tags)
-    {
         psi.ArgumentList.Add("--tag");
-        psi.ArgumentList.Add(tag);
-    }
+        psi.ArgumentList.Add(mainTag);
 
-    if (backup.UseVss)
-    {
-        psi.ArgumentList.Add("--use-fs-snapshot");
-    }
+        foreach (var tag in backup.Tags)
+        {
+            psi.ArgumentList.Add("--tag");
+            psi.ArgumentList.Add(tag);
+        }
 
-    psi.ArgumentList.Add("--compression");
-    psi.ArgumentList.Add("max");
+        if (backup.UseVss)
+        {
+            psi.ArgumentList.Add("--use-fs-snapshot");
+        }
 
-    if (args.Contains("--dry-run"))
-    {
-        psi.ArgumentList.Add("--dry-run");
-    }
-    
-    foreach (var arg in backup.AdditionalArgs)
-    {
-        psi.ArgumentList.Add(arg);
-    }
+        psi.ArgumentList.Add("--compression");
+        psi.ArgumentList.Add("max");
 
-    Console.WriteLine($"""
-                       cd {psi.WorkingDirectory}
-                       """);
-    Console.WriteLine($"""
-                       restic "{string.Join(@""" """, psi.ArgumentList)}"
-                       """);
+        if (args.Contains("--dry-run"))
+        {
+            psi.ArgumentList.Add("--dry-run");
+        }
 
-    using var proc = Process.Start(psi)!;
-    await proc.WaitForExitAsync();
+        foreach (var arg in backup.AdditionalArgs)
+        {
+            psi.ArgumentList.Add(arg);
+        }
 
-    if (proc.ExitCode != 0 && !args.Contains("--ignore-errors"))
-    {
-        throw new InvalidOperationException("Restic Backup process has exited with code " + proc!.ExitCode);
+        Console.WriteLine($"""
+                           cd {psi.WorkingDirectory}
+                           """);
+        Console.WriteLine($"""
+                           restic "{string.Join(@""" """, psi.ArgumentList)}"
+                           """);
+
+        using var proc = Process.Start(psi)!;
+        await proc.WaitForExitAsync();
+
+        if (proc.ExitCode != 0 && !args.Contains("--ignore-errors"))
+        {
+            throw new InvalidOperationException("Restic Backup process has exited with code " + proc.ExitCode);
+        }
     }
 }
-
-Console.WriteLine("Press any key to exit...");
-Console.ReadKey();
+finally
+{
+    Console.WriteLine("Press any key to exit...");
+    Console.ReadKey();
+}
